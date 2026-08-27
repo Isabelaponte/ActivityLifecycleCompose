f9879a8dc091c0bec6fff74b0cde17700857c911
Isabela de Ponte <isabelapnt01@gmail.com>
2026-08-26 21:50:35 -0300

    utilização de savedStateHandle para salvar dados mesmo que o estado morra

--- app/src/main/java/br/edu/ifsp/scl/sc3035018/activitylifecyclecompose/MainViewModel.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3035018/activitylifecyclecompose/MainViewModel.kt
@@ -4,24 +4,31 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class MainViewModel: ViewModel() {
-    private val _uiState = MutableStateFlow(User()) //estado mutável será realizado pelo _uiState
+class MainViewModel(val savedStateHandle: SavedStateHandle): ViewModel() {
+    private companion object { //singleton interno, funciona QUASE como um enum pra guardar constancia
+        const val USER_KEY = "user"
+    }
+
+    private val _uiState = MutableStateFlow(savedStateHandle[USER_KEY] ?: User()) //tenta pegar o estado da ultima morte dele, se não ele monta um User vazio
     val uiState: StateFlow<User> = _uiState.asStateFlow() //somente get
 
-    val x = mutableStateOf(0) //pq a mudança da estratégia? nao melhora desempenho, mas é melhor na arquitetura por causa da divisão de responsabilidades
-
     fun updateName(name: String ) {
-        _uiState.value.name = name
+        _uiState.update { it.copy(name = name) } // atualiza a tela
+        savedStateHandle[USER_KEY] = _uiState.value.copy(name = name) // guarda os dados de instancia caso ele morra
+        // savedStateHandle[USER_KEY] = (savedStateHandle[USER_KEY] as? User)?.copy(name = name) ?: User() // assim é melhor por nao estar fazendo gambiarra por pegar pelo state. dessa forma ele pega pelo get, mas por algum motivo ele nao está funcionando (talvez seja versao
+        // savedStateHandle[USER_KEY] = savedStateHandle.get<User>(USER_KEY)?.copy(name = name) ?: User() //desse jeito funciona e é melhor
     }
 
     fun updateAge(age: Int?) {
-        _uiState.update { it.copy(age = age) } //forma melhor de realizar o set algo
+        _uiState.update { it.copy(age = age) }
+        savedStateHandle[USER_KEY] = _uiState.value.copy(age = age)
     }
 
 }
--- app/src/main/java/br/edu/ifsp/scl/sc3035018/activitylifecyclecompose/User.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3035018/activitylifecyclecompose/User.kt
@@ -1,6 +1,10 @@
 package br.edu.ifsp.scl.sc3035018.activitylifecyclecompose
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize //faz com que os dados de instancia do viewmodel sobrevivam a morte do processo (nenhuma das outras estrategias utilizadas anteriormente faziam isso)
 data class User(
     var name: String = "",
     var age: Int? = 0
-)
+): Parcelable
